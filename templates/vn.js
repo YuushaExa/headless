13bc0aefdb569a3517fa53ae97ff7acd10c2f3ac
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-03-22 01:13:29 +0300

    Update vn.js

--- templates/vn.js
+++ templates/vn.js
@@ -1,13 +1,36 @@
 module.exports = {
-  basePath: 'vn', // Base path for generated files
-  pageSize: 10,   // Number of items per page
-  dataUrl: 'https://raw.githubusercontent.com/YuushaExa/testapi/refs/heads/main/merged.json', // Data URL specific to this template
-
-  // Extract entities (posts and developers)
-  extractEntities: (data) => {
+  basePath: 'vn',
+  itemMapper: (post) => ({
+    id: post.id,
+    title: post.title,
+    developers: post.developers?.map((developer) => ({
+      name: developer.name,
+      id: developer.id,
+      link: `vn/developers/${developer.id}.json`,
+    })),
+    aliases: post.aliases || [],
+    description: post.description || null,
+    image: post.image || null,
+    link: `vn/posts/${post.id}.json`,
+  }),
+  pageMapper: (pagePosts, currentPage, totalPages) => ({
+    posts: pagePosts.map((post) => ({
+      id: post.id,
+      title: post.title,
+      image: post.image || null,
+      link: `vn/posts/${post.id}.json`,
+    })),
+    pagination: {
+      currentPage,
+      totalPages,
+      nextPage: currentPage < totalPages ? `vn/posts/page/${currentPage + 1}.json` : null,
+      previousPage: currentPage > 1 ? (currentPage === 2 ? 'vn/posts/index.json' : `vn/posts/page/${currentPage - 1}.json`) : null,
+    },
+  }),
+  extractRelatedEntities: (posts) => {
     const developersMap = {};
 
-    data.forEach((post) => {
+    posts.forEach((post) => {
       post.developers?.forEach((developer) => {
         const developerId = developer.id;
         if (!developersMap[developerId]) {
@@ -26,69 +49,6 @@ module.exports = {
       });
     });
 
-    return {
-      posts: data.map((post) => ({
-        id: post.id,
-        title: post.title,
-        developers: post.developers?.map((developer) => ({
-          name: developer.name,
-          id: developer.id,
-          link: `vn/developers/${developer.id}.json`,
-        })),
-        aliases: post.aliases || [],
-        description: post.description || null,
-        image: post.image || null,
-        link: `vn/posts/${post.id}.json`,
-      })),
-      developers: Object.values(developersMap),
-    };
-  },
-
-  // Generate paginated files for posts and developers
-  generateFiles: async ({ fetchData, generatePaginatedFiles }) => {
-    // Fetch data using the template-specific URL
-    const data = await fetchData(this.dataUrl);
-    const { posts, developers } = this.extractEntities(data);
-
-    // Generate paginated files for posts
-    await generatePaginatedFiles({
-      items: posts,
-      pageSize: this.pageSize,
-      basePath: `${this.basePath}/posts`,
-      itemMapper: (post) => post,
-      pageMapper: (pagePosts, currentPage, totalPages) => ({
-        posts: pagePosts.map((post) => ({
-          id: post.id,
-          title: post.title,
-          image: post.image || null,
-          link: `${this.basePath}/posts/${post.id}.json`,
-        })),
-        pagination: this.generatePaginationLinks(currentPage, totalPages, `${this.basePath}/posts`),
-      }),
-    });
-
-    // Generate paginated files for developers
-    await generatePaginatedFiles({
-      items: developers,
-      pageSize: this.pageSize,
-      basePath: `${this.basePath}/developers`,
-      itemMapper: (developer) => developer,
-      pageMapper: (pageDevelopers, currentPage, totalPages) => ({
-        developers: pageDevelopers.map((dev) => ({
-          id: dev.id,
-          name: dev.name,
-          link: `${this.basePath}/developers/${dev.id}.json`,
-        })),
-        pagination: this.generatePaginationLinks(currentPage, totalPages, `${this.basePath}/developers`),
-      }),
-    });
-  },
-
-  // Pagination links generator
-  generatePaginationLinks: (currentPage, totalPages, basePath) => {
-    const nextPage = currentPage < totalPages ? `${basePath}/page/${currentPage + 1}.json` : null;
-    const previousPage = currentPage > 1 ? (currentPage === 2 ? 'index.json' : `${basePath}/page/${currentPage - 1}.json`) : null;
-
-    return { currentPage, totalPages, nextPage, previousPage };
+    return Object.values(developersMap);
   },
 };
