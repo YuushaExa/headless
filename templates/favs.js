8d0d099fca7331ceb8b8e6cd24f3204ca903bdb9
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-04-03 00:36:54 +0300

    Update favs.js

--- templates/favs.js
+++ templates/favs.js
@@ -15,26 +15,31 @@ module.exports = {
   basePath: 'favs/posts',
   dataUrl: 'https://raw.githubusercontent.com/YuushaExa/merge/main/favsr/merged.json',
 
-  itemMapper: (post) => ({
-    title: post.title || null,
-    image: post.url || null,
-    link: `favs/posts/${this.plugins.slugify(post.title)}.json`,
-  }),
-
-  pageMapper: (pagePosts, currentPage, totalPages) => ({
-    posts: pagePosts.map(post => ({
+  itemMapper: function(post) {
+    return {
       title: post.title || null,
       image: post.url || null,
       link: `favs/posts/${this.plugins.slugify(post.title)}.json`,
-    })),
-    pagination: {
-      currentPage,
-      totalPages,
-      nextPage: currentPage < totalPages ? `favs/posts/page/${currentPage + 1}.json` : null,
-      previousPage: currentPage > 1 ? (currentPage === 2 ? 'favs/posts/index.json' : `favs/posts/page/${currentPage - 1}.json`) : null,
-    },
-  }),
+    };
+  },
 
-  // Only specify how to generate filenames
-  fileNameGenerator: (item) => `${this.plugins.slugify(item.title)}.json`,
+  pageMapper: function(pagePosts, currentPage, totalPages) {
+    return {
+      posts: pagePosts.map(post => ({
+        title: post.title || null,
+        image: post.url || null,
+        link: `favs/posts/${this.plugins.slugify(post.title)}.json`,
+      })),
+      pagination: {
+        currentPage,
+        totalPages,
+        nextPage: currentPage < totalPages ? `favs/posts/page/${currentPage + 1}.json` : null,
+        previousPage: currentPage > 1 ? (currentPage === 2 ? 'favs/posts/index.json' : `favs/posts/page/${currentPage - 1}.json`) : null,
+      },
+    };
+  },
+
+  fileNameGenerator: function(item) {
+    return `${this.plugins.slugify(item.title)}.json`;
+  },
 };
