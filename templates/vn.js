44ea27b6de48cb0ae933ce60dfc44f7bd0214346
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-06-24 00:34:56 +0300

    Update vn.js

--- templates/vn.js
+++ templates/vn.js
@@ -38,7 +38,7 @@ module.exports = {
             enabled: true,
             settings: {
                 fieldsToIndex: ['title', 'description'],
-                idField: 'id',
+                idField: 'description',
                 minWordLength: 1,
                 prefixLength: 2,
             }
